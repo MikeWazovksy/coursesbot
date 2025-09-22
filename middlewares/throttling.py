dce5c57314fd9b685e3ecdedcfa9acc10aceccc2
Mike <mikevazovsky@hotmail.com>
2025-09-22 13:33:04 +0300

    Edit Middlewares

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -2,25 +2,29 @@
 
 from typing import Callable, Dict, Any, Awaitable
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from cachetools import TTLCache
 
-
 class ThrottlingMiddleware(BaseMiddleware):
-
     def __init__(self, time_limit: int = 1):
-        # Лимит на запросы к боту , чтоб не спамить его
         self.cache = TTLCache(maxsize=10_000, ttl=time_limit)
 
     async def __call__(
         self,
         handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str, Any],
+        event: Message | CallbackQuery,
+        data: Dict[str, Any]
     ) -> Any:
-        if event.chat.id in self.cache:
+        if isinstance(event, Message):
+            user_id = event.from_user.id
+        elif isinstance(event, CallbackQuery):
+            user_id = event.from_user.id
+        else:
+            return await handler(event, data)
+
+        if user_id in self.cache:
             return
 
-        self.cache[event.chat.id] = None
+        self.cache[user_id] = None
 
         return await handler(event, data)
