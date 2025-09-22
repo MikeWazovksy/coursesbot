7ba6657033e86bbd00dbea8e02c5f205d0e8a47b
Mike <mikevazovsky@hotmail.com>
2025-09-22 03:11:35 +0300

    Update

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 # bot.py
 import logging
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
