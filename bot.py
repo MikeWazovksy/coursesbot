81e499843e0d9501c089dd3031d17695c375eec4
Mike <mikevazovsky@hotmail.com>
2025-09-29 22:23:50 +0300

    Edit http

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import asyncio
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiohttp import web
 
 from config import BOT_TOKEN
 from database import initialize_db, create_pool, close_pool
@@ -15,10 +16,32 @@ APP_HOST = "0.0.0.0"
 APP_PORT = int(os.environ.get("PORT", 8080))
 
 
+# =======================
+# Фиктивный HTTP-сервер для Render , чтоб слушал порт
+# =======================
+async def handle(request):
+    return web.Response(text="Bot is running!")
+
+async def start_dummy_server():
+    app = web.Application()
+    app.add_routes([web.get("/", handle)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, APP_HOST, APP_PORT)
+    await site.start()
+    logging.info(f"Dummy HTTP server running on {APP_HOST}:{APP_PORT}")
+
+
+# =======================
+# Основной код бота
+# =======================
 async def main():
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
+
+    # Запуск фиктивного HTTP-сервера чисто для Render.com
+    asyncio.create_task(start_dummy_server())
 
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await bot.delete_webhook(drop_pending_updates=True)
